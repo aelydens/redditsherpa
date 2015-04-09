bd223d1469c67c5bfd94230e81686ace2da6a3d4
Annie Lydens <aelydens@gmail.com>
2015-04-09 14:48:01 -0600

    cleanup and fixes. Remove thor

--- lib/redditsherpa/cli.rb
+++ lib/redditsherpa/cli.rb
@@ -1,14 +1,11 @@
-require 'thor'
 require 'redditsherpa/client'
 require 'launchy'
 require 'rainbow/ext/string'
-require 'pry'
 
 module Redditsherpa
-  class CLI < Thor
+  class CLI
     include Launchy
 
-    desc "search TOPIC", "Search reddit for a specific topic"
     def search(topic)
       response = client.search(topic)
       result = response[:data][:children]
@@ -21,9 +18,9 @@ module Redditsherpa
           puts "_____________________________________________"
         end
       end
+      puts ("Search for something else or read a subreddit! Type 'read SUBREDDIT' with a specific subreddit or 'surprise me' for a random subreddit. Or type help for more.").color(:cyan)
     end
 
-    desc "read SUBREDDIT", "Get the top posts from a given subreddit"
     def read(subreddit)
       puts "reading subreddit..."
       response = client.get_posts(subreddit)
@@ -41,39 +38,37 @@ module Redditsherpa
         puts
         i += 1
       end
+      puts ("To look at comments for a specific topic in the console, type 'comments TOPICNUMBER'. Ex. 'comments 25'").color(:cyan)
+      puts ("To open a link in the browser, type 'open TOPICNUMBER'. Ex. 'open 12'").color(:cyan)
+      puts ("Done browsing? Type 'exit' or 'x' to quit redditsherpa").color(:cyan)
+    end
 
-      puts "To open a page, please enter a topic number after 'open'. Example: open 12"
-      puts "To open the comments for a specific topic in terminal, enter 'c' and then the topic number. Example: c 12"
+    def comments(topicnumber)
+      comments_url = @array[topicnumber.to_i - 1] +".json"
+      response2 = Faraday.get comments_url
+      json_response2 = JSON.parse(response2.body)
 
-      input = STDIN.gets.chomp!
-      if input.include?("open")
-        input = input.gsub("open ","")
-        target_url = @array[input.to_i - 1]
-        puts "Opening #{target_url}..."
-        Launchy.open(target_url)
-      else
-        input.include?("c")
-        input = input.gsub("c ","")
-        comments_url = @array[input.to_i - 1] +".json"
+      thread = json_response2[0]["data"]["children"][0]
+      puts
+      puts "***************************COMMENTS***************************"
+      puts
+      puts "______________________________________________________________"
+      puts Rainbow("Title: #{thread["data"]["title"]}").magenta.bright
+      puts "Author: #{thread["data"]["author"]}"
+      puts "Number of Comments: #{thread["data"]["num_comments"]}"
+      puts "______________________________________________________________"
 
-        response2 = Faraday.get comments_url
-        json_response2 = JSON.parse(response2.body)
-
-        thread = json_response2[0]["data"]["children"][0]
-        puts
-        puts "***************************COMMENTS***************************"
-        puts
-        puts "______________________________________________________________"
-        puts Rainbow("Title: #{thread["data"]["title"]}").magenta.bright
-        puts "Author: #{thread["data"]["author"]}"
-        puts "Number of Comments: #{thread["data"]["num_comments"]}"
-        puts "______________________________________________________________"
-
-        top_level_comments = json_response2[1]["data"]["children"]
-        top_level_comments.each do |comment|
-          print_subcomments_recursively(comment)
-        end
+      top_level_comments = json_response2[1]["data"]["children"]
+      top_level_comments.each do |comment|
+        print_subcomments_recursively(comment)
       end
+      puts ("Read another subreddit! Type 'read SUBREDDIT' with a specific subreddit or 'surprise me' for a random subreddit.").color(:cyan)
+    end
+
+    def open_up(topicnumber)
+      target_url = @array[topicnumber.to_i - 1]
+      puts "Opening #{target_url}..."
+      Launchy.open(target_url)
     end
 
     private
@@ -107,12 +102,12 @@ while true
     begin
       input = STDIN.gets.chomp
       if input == 'help'
-        puts "Useful redditsherpa commands"
-        puts "-" * 20
-        puts "search WORD - search reddit for stuff"
-        puts "read SUBREDDIT - read a specific subreddit"
-        puts "x or exit - quit redditsherpa"
-        puts "surprise me - read a randomly selected subreddit"
+        puts ("Useful redditsherpa commands").color(:cyan)
+        puts ("-" * 20).color(:cyan)
+        puts ("search WORD - search reddit for stuff").color(:cyan)
+        puts ("read SUBREDDIT - read a specific subreddit").color(:cyan)
+        puts ("x or exit - quit redditsherpa").color(:cyan)
+        puts ("surprise me - read a randomly selected subreddit").color(:cyan)
         puts "-" * 20
 
       elsif input.include?('search')
@@ -123,15 +118,23 @@ while true
         subreddit = input.gsub('read ', '').strip
         cli.read(subreddit)
 
+      elsif input.include?('comments')
+        topicnumber = input.gsub('comments ', '').strip
+        cli.comments(topicnumber)
+
       elsif input == 'surprise me'
         subreddits = ["funny", "AdviceAnimals", "todayilearned", "WTF", "leagueoflegends", "aww", "AskReddit", "mildlyinteresting"]
         subreddit = subreddits.sample
         cli.read(subreddit)
 
+      elsif input.include?('open')
+        topicnumber = input.gsub('open ', '').strip
+        cli.open_up(topicnumber)
+
       elsif input == 'x' || input == 'exit'
         exit
       else
-        puts "Your command is not recognized. Please type help to see a list of approved commands."
+        puts ("Your command is not recognized. Please type help to see a list of approved commands.").color(:red)
       end
     rescue
       puts "
@@ -140,7 +143,7 @@ while true
 ..........................................$Z+.   .... ?ZZZZZZ, .  .... .    ..
 ......YOU DONE MESSED UP................ $ZZZZZZZZZ+.ZZZ. .,ZZ?. ..... .  .. . .
 .......  . .. .... .  . .......... ......ZZ .. .,IZZZZZ   . .ZZ. ..... .. . .. .
-.Type h to see a list of accepted ....7Z$ ........=ZZ  ....ZZ.................
+.Type help to see a list of accepted ...7Z$ ........=ZZ  ....ZZ.................
 .....  commands!!!!       .             ZZ..... .... ZZ7 .. ZZ7 ................
 ...                                    $Z$ ...........$ZZZZZZ+  ................
 . ...Stop breaking shit.                 ZZ  .............,==.... .... .........
