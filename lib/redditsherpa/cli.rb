d929ab7581de71d19ecc3851a65d8b38d37ecd76
Annie Lydens <aelydens@gmail.com>
2015-04-08 23:53:10 -0600

    Add Rainbow to output

--- lib/redditsherpa/cli.rb
+++ lib/redditsherpa/cli.rb
@@ -2,19 +2,23 @@ require 'thor'
 require 'redditsherpa/client'
 require 'launchy'
 require 'pry'
+require 'rainbow'
 
 module Redditsherpa
   class CLI < Thor
     include Launchy
+    include Rainbow
 
     desc "search TOPIC", "Search for content on reddit by passing in a topic"
     def search(topic)
       response = client.search(topic)
       result = response[:data][:children]
+      puts "***********************************"
+      puts Rainbow("Search Results:").magenta.underline.bright
       result.each_with_index do |hash, i|
         unless i == 0
-          puts hash[:data][:title]
-          puts hash[:data][:description]
+          puts Rainbow(hash[:data][:title]).green
+          puts (hash[:data][:description])
           puts "_____________________________________________"
         end
       end
@@ -31,13 +35,14 @@ module Redditsherpa
         post = post[:data]
         post_id = post[:id]
         @array << "http://www.reddit.com/r/#{subreddit}/comments/#{post_id}/"
-        puts "#{i}. " + post[:title]
+        puts Rainbow("#{i}. " + post[:title]).bright
         puts "#{post[:url]}"
         puts "____________________________________________________________"
-        puts "Upvotes: #{post[:ups]} | Downvotes: #{post[:downs]} | Number of Comments #{post[:num_comments]}"
-        puts ""
+        puts Rainbow("Upvotes: #{post[:ups]}").green + "|" + Rainbow("Downvotes: #{post[:downs]}").red + "| Number of Comments #{post[:num_comments]}"
+        puts
         i += 1
       end
+
       puts "To open a page, please enter a topic number after 'open'. Example: open 12"
       puts "To open the comments for a specific topic in terminal, enter 'c' and then the topic number. Example: c 12"
 
--- redditsherpa.gemspec
+++ redditsherpa.gemspec
@@ -22,6 +22,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'pry'
   spec.add_dependency 'faraday'
   spec.add_dependency 'launchy'
+  spec.add_dependency 'rainbow'
 
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
